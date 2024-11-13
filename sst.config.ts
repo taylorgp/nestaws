73a073ec4ec474bf6c781c8b3512ec4fee2cc773
Taylor Murray <taylor@goodparty.org>
2024-11-13 10:18:03 -0800

    feat: update sst config.

--- sst.config.ts
+++ sst.config.ts
@@ -8,5 +8,17 @@ export default $config({
       home: 'aws',
     };
   },
-  async run() {},
+  async run() {
+    const vpc = new sst.aws.Vpc('NestVpc', { bastion: false });
+    const cluster = new sst.aws.Cluster('NestCluster', { vpc });
+
+    cluster.addService('NestService', {
+      loadBalancer: {
+        ports: [{ listen: '80/http' }],
+      },
+      dev: {
+        command: 'node --watch index.js',
+      },
+    });
+  },
 });
